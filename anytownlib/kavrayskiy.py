be1e5d511f53c77f4b3441c9d986f317e66e991f
Andrew Yang <ayang14@illinois.edu>
2016-10-31 23:40:47 -0500

    Make global level image function

--- anytownlib/kavrayskiy.py
+++ anytownlib/kavrayskiy.py
@@ -1,6 +1,7 @@
 """Functions that produce a Kavrayskiy VII projection map."""
 
 import math
+from PIL import Image, ImageDraw
 
 
 def coords_to_kavrayskiy(coords):
@@ -15,3 +16,24 @@ def coords_to_kavrayskiy(coords):
     x = (3 * lng / 2) * math.sqrt((1 / 3.) - (lat / math.pi)**2)
     y = lat
     return (x, y)
+
+
+def draw_red_dot_on_map(im, coords, color='red', radius=20):
+    """Draw a red dot at the given geographical coordinates on the image."""
+    width, height = im.size
+    x, y = coords_to_kavrayskiy(coords)
+    x = (x / (math.sqrt(3) * math.pi)) + 0.5
+    x *= width
+    y = (-y / math.pi) + 0.5
+    y *= height
+
+    draw = ImageDraw.Draw(im)
+    draw.ellipse((x - radius, y - radius, x + radius, y + radius),
+                 fill=color, outline=color)
+
+
+def make_global_level_image(coords):
+    """Make a global level image and put a red dot on the given geocoords."""
+    im = Image.open('kav7.png')
+    draw_red_dot_on_map(im, coords)
+    im.show()
