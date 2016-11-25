687ed9bac28548399461338528f708533e74d5ea
Andrew Yang <ayang14@illinois.edu>
2016-11-24 23:07:33 -0600

    Implement update_map_cache function

--- anytownlib/map_cache.py
+++ anytownlib/map_cache.py
@@ -4,4 +4,10 @@
 def update_map_cache(
         db, place_id, coords, city, region, country_name, country_code):
     """Upsert map cache row in the database."""
+    cur = db.cursor()
     lat, lng = coords
+    cur.execute(
+        '''UPDATE map_cache SET lat=?, lng=?, city=?, region=?,
+        country_name=?, country_code=? WHERE place_id=?''',
+        (lat, lng, city, region, country_name, country_code, place_id))
+    db.commit()
