098cdb6f687f4f2f976ba28ed32c9e49a4e66785
Andrew Yang <ayang14@illinois.edu>
2016-11-26 01:50:06 -0600

    Add foreign key pragma

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(app.config['DATABASE'])
         db.row_factory = sqlite3.Row
+        db.execute('PRAGMA foreign_keys = ON')
     return db
 
 
